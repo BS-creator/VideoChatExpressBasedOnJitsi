ed68f92a7afc65aba7143639ab4c38a5a4a86183
BS-creator <58999791+BS-creator@users.noreply.github.com>
2012-04-21 12:08:49 +0800

    'add_fontawesome_and_change_cert'

--- public/javascripts/videochat.js
+++ public/javascripts/videochat.js
@@ -1,13 +1,15 @@
-const domain = 'videochat-jts-aws.s-t-r-i-v-e.com';
+const domain = 'resource.bemycall.com';
 console.log(document.getElementById("roomName").innerHTML)
 let roomName = document.getElementById("roomName").innerHTML;
+let call_type = roomName.charAt(roomName.length - 1);
+
 const options = {
   roomName: roomName,
   width: "100%",
   height: "100%",
   interfaceConfigOverwrite: {
     // MOBILE_APP_PROMO: false,
-    DEFAULT_REMOTE_DISPLAY_NAME: 'New One',
+    DEFAULT_REMOTE_DISPLAY_NAME: 'Hello',
     SHOW_JITSI_WATERMARK: false,
     SHOW_WATERMARK_FOR_GUESTS: false,
     SHOW_BRAND_WATERMARK: false,
@@ -24,9 +26,10 @@ const options = {
       'closedcaptions',
       'desktop', //sharescreen
       'hangup',
-      'profile',
+      'tileview',
       'chat',
       'etherpad',
+      'profile',
       // 'recording',
       // 'fullscreen',
       // 'fodeviceselection',
@@ -41,7 +44,6 @@ const options = {
       // 'feedback',
       // 'stats',
       // 'shortcuts',
-      // 'tileview',
       // 'videobackgroundblur',
       // 'download',
       // 'help',
@@ -51,35 +53,55 @@ const options = {
   },
   configOverwrite: {
     enableWelcomePage: false,
-    enableClosePage: true,
+    enableClosePage: false,
     disableDeepLinking: true,
     disableSimulcast: false,
-    enableNoisyMicDetection: false,
-    enableNoAudioDetection: false,
-    startWithAudioMuted: true,
+    // enableNoisyMicDetection: false,
+    // enableNoAudioDetection: false,
+    // startWithAudioMuted: true,
 
   },
   parentNode: document.querySelector('#meet'),
-  onload: onloadVideoChat,
+  onload: onload_fn,
 };
 
-const api = new JitsiMeetExternalAPI(domain, options);
+if (call_type == 'a') {
+  options.configOverwrite.startAudioOnly = true;
+} else if (call_type == 'm') {
+  options.configOverwrite.startWithAudioMuted = true;
+  options.configOverwrite.startWithVideoMuted = true;
+}
 
-function onloadVideoChat(e) {
-  const iframe = api.getIFrame();
-  console.log(iframe)
-  console.log('onloadvideocaht', e)
-  // var iframe = document.getElementById("jitsiConferenceFrame0");
-  var elmnt = iframe.contentWindow.document.getElementsByTagName("H1")[0];
-  // elmnt.style.display = "none";
-  console.log(elmnt)
-}
-api.addEventListener("readyToClose", readyToCloseEvent);
-function readyToCloseEvent() {
+console.log(options.configOverwrite)
+
+const api = new MeetExternalAPI(domain, options);
+
+api.addEventListener("readyToClose", function () {
   console.log('readyToCloseEvent');
+  // window.location.href = "/left_meet"
   window.location.href = "/"
-}
+});
+
+
+api.addEventListener("videoConferenceJoined", function (data) {
+  console.log('videoconferencejoined', data);
+  if (call_type == 'm') {
+    api.executeCommand('toggleChat');
+  }
+})
 
 api.getAvailableDevices().then(devices => {
   console.log(devices)
 });
+
+function wait(delayInMS) {
+  return new Promise(resolve => setTimeout(resolve, delayInMS));
+}
+
+function onload_fn() {
+  // if (call_type == 'm') {
+  //   wait(2000).then(function () {
+  //     api.executeCommand('toggleChat');
+  //   })
+  // }
+}
--- routes/index.js
+++ routes/index.js
@@ -11,4 +11,8 @@ router.get('/r/:roomName', function (req, res, next) {
   res.render('room', { roomName: req.params.roomName });
 });
 
+/* GET after left page. */
+router.get('/left_meet', function (req, res, next) {
+  res.render('left', { title: 'Video Chat' });
+});
 module.exports = router;
